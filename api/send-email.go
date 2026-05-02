430e202578e68a7c6e8f26cc59ab122efaa4ef6c
Guilherme Matte <guiguimatte@gmail.com>
2026-05-02 15:10:02 -0300

    test

--- api/send-email.go
+++ api/send-email.go
@@ -2,31 +2,16 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 )
 
-type EmailPayload struct {
-	To      string `json:"to"`
-	Subject string `json:"subject"`
-	Body    string `json:"body"`
+type Payload struct {
+	Message string `json:"message"`
 }
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
+	var p Payload
+	json.NewDecoder(r.Body).Decode(&p)
 
-	var payload EmailPayload
-	err := json.NewDecoder(r.Body).Decode(&payload)
-	if err != nil {
-		http.Error(w, "Erro ao realizar parse do JSON", http.StatusBadRequest)
-		return
-	}
-
-	fmt.Printf("Recebido webhook para: %s\n", payload.To)
-
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Webhook processado pela Vercel!"))
+	w.Write([]byte("ok"))
 }
