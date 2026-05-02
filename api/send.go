55ca85bdcc76eb579cbebd3e18457f32929dee5b
Guilherme Matte <guiguimatte@gmail.com>
2026-05-02 14:51:18 -0300

    ajuste send.go

--- api/send.go
+++ api/send.go
@@ -15,29 +15,18 @@ type EmailPayload struct {
 func Handler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		fmt.Printf("Método HTTP inválido! Esperado: POST, recebido: %s", r.Method)
 		return
 	}
 
 	var payload EmailPayload
-
 	err := json.NewDecoder(r.Body).Decode(&payload)
 	if err != nil {
-		http.Error(w, "Erro ao realizar parse do JSON", http.StatusBadRequest)
+		http.Error(w, "Erro no JSON", http.StatusBadRequest)
 		return
 	}
-	fmt.Printf("Lendo da fila: Enviando e-mail para %s...\n", payload.To)
-	err = sendEmail(payload)
 
-	if err != nil {
-		http.Error(w, "Falha ao enviar e-mail", http.StatusInternalServerError)
-		return
-	}
+	fmt.Printf("Processando e-mail para %s\n", payload.To)
+
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Email enviado com sucesso!"))
-}
 
-func sendEmail(p EmailPayload) error {
-	fmt.Printf("Assunto: %s | Conteúdo: %s\n", p.Subject, p.Body)
-	return nil
 }
